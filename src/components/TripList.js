730afbdb37cf927bb69ed36a83b0a8156b4b3f58
NawafAlmaqaadi <nalmaqaadi@stu.kau.edu.sa>
2022-01-27 21:31:26 +0300

    done

---
+++ src/components/TripList.js
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import './TripList.css'
+import { useFetch } from '../hooks/useFetch'
+export default function TripList(){
+    
+    const [url,setUrl] = useState('http://localhost:3000/trips')
+    const { data:trips } = useFetch(url)
+  
+    
+
+    return (
+        <div className="trip-list">
+            <h2>Trip List</h2>
+            <ul>
+                {trips && trips.map((trip)=>(
+                    <li key={trip.id}>
+                        <h3>{trip.title}</h3>
+                        <p>{trip.price}</p>
+                    </li>
+                ))}
+            </ul>
+            <div className="filters">
+            <button onClick={()=> setUrl('http://localhost:3000/trips?loc=europe')}>
+                europe trips
+            </button>
+            <button onClick={()=> setUrl('http://localhost:3000/trips')}>
+                All trips
+            </button>
+            </div>
+        </div>
+    )
+}
